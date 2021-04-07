9c280432971528214a014ac86814305790e9626f
vim77 <derwikinger@yandex.ru>
2021-04-07 20:32:55 +0300

    Create command 'users:all'

---
+++ app/Console/Commands/UsersAll.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AppUser;
+use Illuminate\Console\Command;
+
+class UsersAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Returns all application users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = AppUser::all(['name', 'login', 'email'])->toArray();
+        $headers = ['Name', 'Login', 'Email'];
+
+        $this->table($headers, $users);
+        return 0;
+    }
+}

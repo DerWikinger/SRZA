557c7888a74bcf5b0880f159a2f67231e9b8fc24
vim77 <derwikinger@yandex.ru>
2021-05-03 15:21:22 +0300

    Added 'layouts.logo' to 'layouts.header'

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,21 @@
 <header>
-    <h2>My Application</h2>
-    <h3>
-        @yield('subheader')
-    </h3>
+    <div class="container">
+        <div class="row">
+            @component('layouts.logo')
+                @slot('title')
+                    This is a discription of Logo
+                @endslot
+            @endcomponent
+            <div class="col-md-6">
+                <h2>My Application</h2>
+                <div class="sub-header">
+                    @yield('subheader')
+                </div>
+            </div>
+            <div class="col-md-3">
+            </div>
+        </div>
+    </div>
 </header>
 
 

4ff13d8bd716332227e4cfbf4cb1ed7d2766fa6a
vim77 <vimitrofanov77@gamil.com>
2021-05-08 17:58:14 +0300

    Modified migration '...create_users_table' -> added fields 'role_id' & 'avatar'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,19 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nickname', 50)->nullable();
+            $table->foreignId('role_id')
+                ->references('id')
+                ->on('roles')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
             $table->string('email')->unique();
-            $table->string('nickname')->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

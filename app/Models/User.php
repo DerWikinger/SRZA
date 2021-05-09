b20d0cc32fc206e2a276fcaca76ccf3e8839bc43
vim77 <vimitrofanov77@gamil.com>
2021-05-09 10:02:05 +0300

    Model 'User' modified as 'SoftDeleted'

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthrizableContract;
@@ -22,19 +23,24 @@ class User extends Model implements
     use Authenticatable;
     use Authorizable;
     use MustVerifyEmail;
+    use SoftDeletes;
 
     protected $table = 'users';
 
     protected $fillable = [
-      'name',
-      'email',
-      'nickname',
-      'password'
+        'name',
+        'email',
+        'nickname',
+//        'password',
     ];
 
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    protected $dates = [
+        'deleted_at',
     ];
 
     public function role()
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();

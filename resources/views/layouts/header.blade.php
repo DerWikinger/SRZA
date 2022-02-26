ce9a19a62fdd41ac2344924fe2efdeac55aa8a72
vim77 <vimitrofanov77@gamil.com>
2022-02-26 15:10:35 +0300

    Added 'app-logo' image

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,16 +3,16 @@
         <div class="row">
             @component('layouts.logo')
                 @slot('title')
-                    This is a description of Logo
+{{--                    This is a description of Logo--}}
                 @endslot
             @endcomponent
-            <div class="col-md-6">
-                <h2>My Application</h2>
+            <div class="col-6">
+                <h2>{{ __('app.app-name') }}</h2>
                 <div class="sub-header">
                     @yield('subheader')
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-3">
             </div>
         </div>
     </div>
--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -1,4 +1,4 @@
 <div class="col-md-3">
-    Here must be Logo of Site
+    <img width="60" height="60" src="/storage/images/logo/app-logo-3.png" alt="Логотип">
     <div>{{ $title }}</div>
 </div>

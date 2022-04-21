b9479036d8771533b44867e96e8c63e83e79273d
vim77 <vimitrofanov77@gamil.com>
2022-04-21 14:54:22 +0300

    Modified 'home' view with tilewind classes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,20 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header text-center">{{ __('app.dashboard') }}</div>
-
-                    <div class="card-body text-center">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        @auth
-                            {{ __('app.logged') }}
-                        @endauth
-                        @guest
-                            {{ __('app.not-logged') }}
-                        @endguest
+        <div class="justify-center text-center px-20">
+            <div class="text-2xl my-2">{{ __('app.dashboard') }}</div>
+            <div class="text-lg my-2">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
-                </div>
+                @endif
+                @auth
+                    {{ __('app.logged') }}
+                @endauth
+                @guest
+                    {{ __('app.not-logged') }}
+                @endguest
             </div>
         </div>
     </div>

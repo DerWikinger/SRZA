b3526eedf18a10c62436a160b3e2e748c76e5c10
vim77 <vimitrofanov77@gamil.com>
2022-07-03 14:27:10 +0300

    Modified 'title' tag in the 'master.blade' view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Title -->
-    <title> @yield('title', 'Home page') </title>
+    <title> @yield('title', __('caption.app-name')) </title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
